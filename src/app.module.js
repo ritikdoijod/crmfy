222738b9bd828ea03ea072b208c2434e6b7c4f2f
Ritik Doijod <Ritik_Doijod@magicsoftware.com>
2025-04-21 14:46:50 +0530

    initial commit

--- src/app.module.js
+++ src/app.module.js
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ShopifyModule } from './shopify/shopify.module';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './app.config';
 
 @Module({
-  imports: [ShopifyModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig],
+    }),
+    ShopifyModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+
+export class AppModule {}
--- src/shopify/shopify.controller.js
+++ src/shopify/shopify.controller.js
@@ -1,34 +1,62 @@
-import { Controller, Dependencies, Bind, Req, Res, Get, Post } from '@nestjs/common';
+import {
+  Controller,
+  Dependencies,
+  Bind,
+  Req,
+  Res,
+  Get,
+  Post,
+  HttpCode,
+  Redirect,
+  Query,
+} from '@nestjs/common';
 import { ShopifyService } from './shopify.service';
+import { ConfigService } from '@nestjs/config';
 
-@Controller()
+@Controller('shopify')
+@Dependencies(ConfigService)
 @Dependencies(ShopifyService)
 export class ShopifyController {
-  constructor(shopifyService) {
+  constructor(configService, shopifyService) {
+    this.configService = configService;
     this.shopifyService = shopifyService;
   }
 
-  @Get("/auth/callback")
+  @Get('init')
+  @HttpCode(302)
+  @Redirect()
+  async init(@Query query) {
+    return {
+      url: `https://${query.shop}/admin/oauth/authorize?client_id=${this.configService.get('shopify.appProxy.clientId')}&scope=${this.configService.get('shopify.appProxy.scopes').join(',')}&redirect_uri=${this.configService.get('appUrl')}/shopify/auth/callback&state={nonce}&grant_options[]={access_mode}`,
+    };
+  }
+
+  @Get('/auth/callback')
   @Bind(Req())
   @Bind(Res())
   async handleAuthCallback(req, res) {
     try {
-      const { session } = await this.shopifyService.getShopifyClient().auth.callback({
-        rawRequest: req,
-        rawResponse: res
-      })
+      const { session } = await this.shopifyService
+        .getShopifyClient()
+        .auth.callback({
+          rawRequest: req,
+          rawResponse: res,
+        });
 
-      const response = await this.shopifyService.getShopifyClient().webhooks.register({
-        session
-      })
+      const response = await this.shopifyService
+        .getShopifyClient()
+        .webhooks.register({
+          session,
+        });
 
-      console.log("webhooks response", response);
+      console.log('webhooks response', response);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
 
-  @Post('shopify/webhooks/orders/create')
+  @Post('webhooks/orders/create')
+  @Bind(Req())
   @Bind(Res())
   async handleWebhookOrderCreate(req, res) {
     try {
@@ -36,10 +64,10 @@ export class ShopifyController {
         rawBody: req.body,
         rawRequest: req,
         rawResponse: res,
-      })
+      });
     } catch (error) {
-      console.log(error)
-      // res.status(500).send(error.message)
+      console.log(error);
+      res.status(500).send(error.message)
     }
   }
 }
